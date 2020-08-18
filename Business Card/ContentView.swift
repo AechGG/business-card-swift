450bb85fd39256f14bd4aa3dd9e781a2215e182e
AechGG <hazg@me.com>
2020-08-18 12:46:22 +0100

    feat: add contact details

--- Business Card/ContentView.swift
+++ Business Card/ContentView.swift
@@ -13,10 +13,36 @@ struct ContentView: View {
         ZStack {
             Color(red: 0.00, green: 0.81, blue: 0.79)
                 .edgesIgnoringSafeArea(.all)
-            Text("AechGG")
-                .font(Font.custom("SuisseIntlMono-Regular", size: 40))
-                .foregroundColor(.white)
-                .bold()
+            VStack {
+                Image("Logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.black, lineWidth: 5))
+                Text("AechGG")
+                    .font(Font.custom("SuisseIntlMono-Regular", size: 40))
+                    .foregroundColor(.white)
+                    .bold()
+                Text("Software Engineer").foregroundColor(.white).font(.system(size: 25))
+                Divider()
+                RoundedRectangle(cornerRadius: 40)
+                    .foregroundColor(.white)
+                    .frame(height: 40)
+                    .overlay(Text("AechGG.github.io")
+                        .font(Font.custom("SuisseIntlMono-Regular", size: 15))
+                        .foregroundColor(.black))
+                RoundedRectangle(cornerRadius: 40)
+                    .foregroundColor(.white)
+                    .frame(height: 40)
+                    .overlay(HStack {
+                        Image(systemName: "phone.fill").foregroundColor(Color(red: 0.00, green: 0.81, blue: 0.79))
+                        Text("+44 1234 567891")
+                            .font(Font.custom("SuisseIntlMono-Regular", size: 15))
+                            .foregroundColor(.black)
+                    })
+                
+            }
         }
     }
 }
