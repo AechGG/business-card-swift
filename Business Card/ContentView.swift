2c524654c8d4ee29aa1208b27973efae1db9c22c
AechGG <hazg@me.com>
2020-08-18 12:09:50 +0100

    Initial Commit

---
+++ Business Card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Business Card
+//
+//  Created by Harrison Gittos on 18/08/2020.
+//  Copyright © 2020 Harrison Gittos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
